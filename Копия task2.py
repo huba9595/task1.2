da81dc5091bab0c0016e10ce923462197c05768b
macbook <macbook@macbookdeMacBook-Pro-4.local>
2019-12-20 22:50:09 -0800

    bla bla

---
+++ Копия task2.py
@@ -0,0 +1,11 @@
+#write your code here...
+def square_number(number):
+    return number ** 2
+#DO NOT remove lines below here, this is designed to test your code.
+def test_square_number():
+    assert square_number(2) == 4
+    assert square_number(8) == 64
+    assert square_number(10) == 100
+    print("YOUR CODE IS CORRECT!")
+
+test_square_number()
